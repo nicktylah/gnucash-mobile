ecaa4350d8ab2e1c2ed79e0ceb6cffb013e7c08f
Nick Tyler <nicktyler@Nicks-MacBook-Pro.local>
2021-02-26 14:50:52 -0800

    Allows for picking an accounts CSV from file system
      - Import flow is no longer hard-coded to my accounts, rather a local file can be selected.
      - TODO: test erroneous files

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:gnucash_mobile/providers/accounts.dart';
 import 'package:gnucash_mobile/providers/transactions.dart';
@@ -98,101 +101,116 @@ class _MyHomePageState extends State<MyHomePage> {
               appBar: AppBar(
                 title: Text(widget.title),
               ),
-              body: _hasImported
-                  ? ListOfAccounts(accounts: accounts)
-                  : Intro(),
+              body: _hasImported ? ListOfAccounts(accounts: accounts) : Intro(),
               drawer: Drawer(
                   child: ListView(
-                    padding: EdgeInsets.zero,
-                    children: <Widget>[
-                      DrawerHeader(
-                        child: Text(
-                          "Gnucash Mobile",
-                          style: TextStyle(
-                            color: Constants.lightPrimary,
-                            fontSize: 20,
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          color: Constants.darkBG,
-                        ),
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  DrawerHeader(
+                    child: Text(
+                      "Gnucash Mobile",
+                      style: TextStyle(
+                        color: Constants.lightPrimary,
+                        fontSize: 20,
                       ),
-                      ListTile(
-                          title: Text('Import Accounts'),
-                          onTap: () {
-                            if (_hasImported) {
-                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                content: Text(
-                                    "Accounts already imported. Please remove them first."),
-                              ));
-                            } else {
-                              Provider.of<AccountsModel>(context, listen: false)
-                                  .addAll();
-                            }
-                            Navigator.pop(context);
-                          }),
-                      ListTile(
-                          title: Text('Export'),
-                          onTap: () async {
-                            final _success = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Export(),
-                              ),
-                            );
+                    ),
+                    decoration: BoxDecoration(
+                      color: Constants.darkBG,
+                    ),
+                  ),
+                  ListTile(
+                      title: Text('Import Accounts'),
+                      onTap: () async {
+                        if (_hasImported) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                                "Accounts already imported. Please remove them first."),
+                          ));
+                          Navigator.pop(context);
+                          return;
+                        }
 
-                            if (_success != null && _success) {
-                              Navigator.pop(context);
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text("Transactions exported!")));
-                            }
-                          }),
-                      ListTile(
-                          title: Text('Favorites'),
-                          onTap: () {
-                             Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Favorites(),
-                              ),
-                            );
-                          }),
-                      ListTile(
-                          title: Text('Delete Accounts'),
-                          onTap: () {
+                        FilePickerResult result =
+                            await FilePicker.platform.pickFiles();
+
+                        if (result != null) {
+                          try {
+                            final _file = File(result.files.single.path);
+                            String contents = await _file.readAsString();
                             Provider.of<AccountsModel>(context, listen: false)
-                                .removeAll();
+                                .addAll(contents);
                             Navigator.pop(context);
-                          }),
-                      ListTile(
-                          title: Text('Delete Transactions'),
-                          onTap: () {
-                            Provider.of<TransactionsModel>(context, listen: false)
-                                .removeAll();
-                            Navigator.pop(context);
-                          }),
-                    ],
-                  )),
+                          } catch (e) {
+                            print(e);
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text(
+                                  "Oops, something went wrong while importing. Please correct any errors in your Accounts CSV and try again."),
+                            ));
+                          }
+                        }
+                      }),
+                  ListTile(
+                      title: Text('Export'),
+                      onTap: () async {
+                        final _success = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Export(),
+                          ),
+                        );
+
+                        if (_success != null && _success) {
+                          Navigator.pop(context);
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text("Transactions exported!")));
+                        }
+                      }),
+                  ListTile(
+                      title: Text('Favorites'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Favorites(),
+                          ),
+                        );
+                      }),
+                  ListTile(
+                      title: Text('Delete Accounts'),
+                      onTap: () {
+                        Provider.of<AccountsModel>(context, listen: false)
+                            .removeAll();
+                        Navigator.pop(context);
+                      }),
+                  ListTile(
+                      title: Text('Delete Transactions'),
+                      onTap: () {
+                        Provider.of<TransactionsModel>(context, listen: false)
+                            .removeAll();
+                        Navigator.pop(context);
+                      }),
+                ],
+              )),
               floatingActionButton: _hasImported
                   ? Builder(builder: (context) {
-                return FloatingActionButton(
-                  backgroundColor: Constants.darkBG,
-                  child: Icon(Icons.add),
-                  onPressed: () async {
-                    final _success = await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => TransactionForm(),
-                      ),
-                    );
+                      return FloatingActionButton(
+                        backgroundColor: Constants.darkBG,
+                        child: Icon(Icons.add),
+                        onPressed: () async {
+                          final _success = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TransactionForm(),
+                            ),
+                          );
 
-                    if (_success != null && _success) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text("Transaction created!")));
-                    }
-                  },
-                );
-              })
+                          if (_success != null && _success) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text("Transaction created!")));
+                          }
+                        },
+                      );
+                    })
                   : null,
             );
           });
--- lib/widgets/intro.dart
+++ lib/widgets/intro.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:gnucash_mobile/constants.dart';
 import 'package:gnucash_mobile/providers/accounts.dart';
@@ -7,12 +10,35 @@ class Intro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: FlatButton(
-        child: Text('Import Accounts'),
-        color: Constants.darkAccent,
-        onPressed: () =>
-            Provider.of<AccountsModel>(context, listen: false).addAll(),
-        textColor: Constants.lightPrimary,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all<Color>(Constants.darkAccent),
+        ),
+        onPressed: () async {
+          FilePickerResult result = await FilePicker.platform.pickFiles();
+
+          if (result != null) {
+            try {
+              final _file = File(result.files.single.path);
+              String contents = await _file.readAsString();
+              Provider.of<AccountsModel>(context, listen: false)
+                  .addAll(contents);
+            } catch (e) {
+              print(e);
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(
+                    "Oops, something went wrong while importing. Please correct any errors in your Accounts CSV and try again."),
+              ));
+            }
+          }
+        },
+        child: Text(
+          "Import",
+          style: TextStyle(
+            color: Constants.lightPrimary,
+          ),
+        ),
       ),
     );
   }
